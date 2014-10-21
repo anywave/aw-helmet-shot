1f93f51fad83fa502f7fbce587b26d11503b3841
maedoc <maedoc@mm.st>
2014-10-21 13:22:57 +0200

    complete plugin

--- __main__.py
+++ __main__.py
@@ -1,10 +1,20 @@
 import os, sys, anywave
+import pyqtgraph as pg
 
-print sys.path
+# need matplotlib!
+sys.path.append('/usr/lib/pymodules/python2.7')
 
 
+# plugin deps
 __file__ = sys.argv[-1]
 here = os.path.dirname(__file__)
 sys.path.append(os.path.join(here, 'mne-python'))
 
 import mne
+
+# run
+app = pg.mkQApp()
+raw = mne.io.read_raw_bti(sys.argv[-2], read_header_only=True)
+raw.info['filename'] = sys.argv[-2]
+mne.viz._3d.pg_plot_meg_and_dig(raw)
+app.exec_()
